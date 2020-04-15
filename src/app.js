db833c782c4c184acf0273fd63d51c3a5153d2ff
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2020-04-15 23:25:06 +0200

    Add two new callbacks to try out

--- src/app.js
+++ src/app.js
@@ -8,6 +8,19 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.command('/break', async ({ command, ack, say }) => {
+  // Acknowledge command request
+  await ack();
+
+  await say(`Enjoy your ${command.text} break`);
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
